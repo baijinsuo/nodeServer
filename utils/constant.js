3f586bfe3e1f121ed710bc0f95d636dd57df2a03
white <2573815219@qq.com>
2020-09-17 23:30:25 +0800

    更改配置ip

--- utils/constant.js
+++ utils/constant.js
@@ -2,5 +2,5 @@ module.exports = {
     PWD_SALT: 'w_node_996ait',//秘钥
     PRIVATE_KEY: 'w_node_blog_996ait',//token
     EXPIRESD: 60 * 60 * 24,//token有效期为1天
-    DOMAIN:'http://127.0.0.1:9000/',//域名
+    DOMAIN:'http://49.233.3.220:9000/',//域名
 }
